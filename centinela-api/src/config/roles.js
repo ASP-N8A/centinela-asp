f5e422f08c2a5f562cacfab70f901f4077c25acd
Esteban <eguruceaga@technisys.com>
2020-09-26 17:05:24 -0300

    RF1 - add controller logic  for register endpoint.

--- centinela-api/src/config/roles.js
+++ centinela-api/src/config/roles.js
@@ -1,4 +1,7 @@
-const roles = ['admin', 'developer'];
+const ADMIN_ROLE = 'admin';
+const DEVELOPER_ROLE = 'developer';
+
+const roles = [ADMIN_ROLE, DEVELOPER_ROLE];
 
 const roleRights = new Map();
 roleRights.set(roles[0], ['basic', 'manageInvitations', 'editIssue', 'manageKeys']);
@@ -7,4 +10,6 @@ roleRights.set(roles[1], ['basic']);
 module.exports = {
   roles,
   roleRights,
+  ADMIN_ROLE,
+  DEVELOPER_ROLE,
 };
