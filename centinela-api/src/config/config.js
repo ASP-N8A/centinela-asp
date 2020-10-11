e9cf3070767f6e5ef17219c574a79b932b4a52c5
diego <diegopizzarello@gmail.com>
2020-10-11 19:46:52 -0300

    redis connection

--- centinela-api/src/config/config.js
+++ centinela-api/src/config/config.js
@@ -18,7 +18,9 @@ const envVarsSchema = Joi.object()
     SMTP_USERNAME: Joi.string().description('username for email server'),
     SMTP_PASSWORD: Joi.string().description('password for email server'),
     EMAIL_FROM: Joi.string().description('the from field in the emails sent by the app'),
-    REDIS_URL: Joi.string().required().description('Redis url'),
+    REDIS_HOST: Joi.string().required().description('Redis url'),
+    REDIS_PORT: Joi.number().required().description('Redis port'),
+    REDIS_PASSWORD: Joi.string().description('Redis password'),
   })
   .unknown();
 
@@ -58,6 +60,8 @@ module.exports = {
     from: envVars.EMAIL_FROM,
   },
   redis: {
-    url: envVars.REDIS_URL + (envVars.NODE_ENV === 'test' ? '-test' : ''),
+    host: envVars.REDIS_HOST + (envVars.NODE_ENV === 'test' ? '-test' : ''),
+    port: envVars.REDIS_PORT,
+    password: envVars.REDIS_PASSWORD,
   },
 };
--- centinela-api/src/queue/index.js
+++ centinela-api/src/queue/index.js
@@ -1,5 +1,8 @@
 const Queue = require('bull');
+const config = require('../config/config');
 
-const invitationEmailQueue = new Queue('invitation-email');
+const invitationEmailQueue = new Queue('invitation-email', {
+  redis: { port: config.redis.port, host: config.redis.host, password: config.redis.password },
+});
 
 module.exports.invitationEmailQueue = invitationEmailQueue;
